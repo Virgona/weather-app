35d6bcf4a2431c2b26a9ee910408cf28041b8f53
Tobias Virgona <tobiasvirgona@gmail.com>
2022-09-21 18:03:28 +1000

    city name will now show when searched. added api keys and calls

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,36 @@
-var searchBtn = $('#searching');
+const searchBtn = $('#searching');
+let searchedCity = $("#myInput").val().trim();
+let cityDisplayName = $('#city-name');
+let apiUrl = 'http://api.openweathermap.org/data/2.5/';
+const apiKey = '051f2bb3cfd048b3c5135242dc4d2da7';
 //stores search result in local storage
-searchBtn.click(function () {
-    var searchedCity = $("#myInput").val().trim();
+searchBtn.click(function (event) {
+    event.preventDefault()
+    let searchedCity = $("#myInput").val().trim();
     localStorage.setItem('city', searchedCity);
     console.log(searchedCity);
+    cityDisplayName.html(searchedCity);
     makeButton()
+
 });
 
 // when the search bar is used. stores the searched city as a button
 
 function makeButton() {
-    var previouslySearched = localStorage.getItem('city')
+    let previouslySearched = localStorage.getItem('city')
     $('#previous-cities').append('<button type="button" class="btn btn-sm btn-secondary button-search" id=" ">' + previouslySearched + '</button>');
+    cityWeather()
 }
 
 //after a city has been input. grabs info from the openWeather API
+
+function cityWeather() {
+    let searchedCity = $("#myInput").val().trim();
+    let cityWeatherURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=1&appid=" + apiKey;
+    fetch(cityWeatherURL)
+        .then(function (response) {
+            console.log(response);
+            console.log(cityWeatherURL);
+            return response.json();
+        });
+}
