a89cb83b8c660245247e962c360f94fe0b037384
Tobias Virgona <tobiasvirgona@gmail.com>
2022-09-22 16:45:31 +1000

    date now shows next to current city and icon related to conditions

--- assets/script.js
+++ assets/script.js
@@ -11,18 +11,26 @@ searchBtn.click(function (event) {
     console.log('once clicked, searchedCity contains: ', searchedCity)
     localStorage.setItem('city', searchedCity);
     console.log(searchedCity);
-    // cityDisplayName.html(searchedCity);
+    $('#next-5-days').html(' ');
     makeButton()
 
 });
+
 
 // when the search bar is used. stores the searched city as a button
 
 function makeButton() {
     let previouslySearched = localStorage.getItem('city')
-    $('#previous-cities').append('<button type="button" class="btn btn-sm btn-secondary button-search" id=" ">' + previouslySearched + '</button>');
+    $('#previous-cities').append('<button type="button" class="btn btn-sm btn-secondary button-search previous">' + previouslySearched + '</button>');
     cityWeather()
+
+    let lastCityBtn = $('.previous');
+    lastCityBtn.click(function () {
+        searchedCity = $('.previous').text();
+        console.log(searchedCity)
+    })
 }
+
 
 //after a city has been input. grabs info from the openWeather API
 
@@ -41,9 +49,13 @@ function cityWeather() {
             let lon = coordData.coord.lon;
             // console.log(lat, lon);
             cityDisplayName.html(coordData.name);
-            $('#temp').html(coordData.main.temp);
+            $('#temp').html(coordData.main.temp + '&#8451;');
             $('#humidity').html(coordData.main.humidity + ' %');
             $('#wind').html(coordData.wind.speed + ' MPH');
+
+            let iconCode = coordData.weather[0].icon;
+            let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            $('#wicon').attr('src', iconUrl);
 
             fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
                 .then(function (response) {
@@ -57,8 +69,18 @@ function cityWeather() {
 
                     if (uvIndexValue > 5) {
                         $('#uv').css('background-color', 'red')
+
+                        if (uvIndexValue > 2) {
+                            $('#uv').css('background-color', 'orange')
+                        }
                     } else $('#uv').css('background-color', 'green')
+
+                    let currentDate = uvData.date_iso.slice(0, 10)
+                    cityDisplayName.append(' (' + currentDate + ')')
+
                 })
+
+
 
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&units=metric&appid=${apiKey}`)
                 .then(function (response) {
@@ -71,10 +93,17 @@ function cityWeather() {
                     let fiveDayForecast = forecastData.list.length;
 
                     for (let i = 0; i < fiveDayForecast; i++) {
-                        $('#next-5-days').append(`<div class="card bg-light mb-3" style="max-width: 18rem;"><div class="card-header"></div><div class="card-body"><h5 class="card-title"></h5><p class="card-text"></p></div>`)
-                        // $('.card-header').html(forecastData.list[0].dt_text)
-                        // $('.card-body').append(`<div>Temp: forecastData.list[0].main.temp </br> Wind: forecastData.list[0].wind.speed MPH`)
+                        $('#next-5-days').append(`<div class="card bg-light mb-3"><div class="card-header"></div><div class="card-body"><h5 class="card-title"></h5><p class="card-text"></p></div>`)
                     }
+
+                    // let forecastDates = forecastData.list[0]
+
+                    // for (const date of forecastDates)
+                    //     $('.card-header').text(forecastData.list[0].dt_txt)
+                    // console.log(forecastData.list[0].dt_txt)
+                    // forecastDates++;
+                    // $('.card-body').append(`<div>Temp: forecastData.list[0].main.temp </br> Wind: forecastData.list[0].wind.speed MPH`)
+                    // forecastData.list++;
                 })
 
 
